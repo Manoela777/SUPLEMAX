28af767b34417c4b90a7b87e404e7e92dfeeae1b
Manoela <mano.cpb@gmail.com>
2026-05-24 14:57:45 -0300

    Mudanças realizadas no dia 23/05

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function calculadora(){
+const calc = document.getElementById('calculadora');
+   calc.classList.toggle("ativo");  
+}
+
+function calcular(){
+    const peso = document.getElementById('peso').value
+    const altura = document.getElementById('altura').value
+
+    console.log(peso)
+    console.log(altura)
+}
